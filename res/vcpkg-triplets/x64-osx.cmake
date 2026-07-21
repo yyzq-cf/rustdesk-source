511dc51b77822e021d1f44f1a239302a66edf561
有云转晴 <yyzq-cf@users.noreply.github.com>
2026-07-21 16:44:14 +0800

    Fix macOS x86 cross-compile: add -arch x86_64 to C/CXX flags in custom triplet for correct __pie__ macros

--- res/vcpkg-triplets/x64-osx.cmake
+++ res/vcpkg-triplets/x64-osx.cmake
@@ -3,8 +3,11 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
-# Cross-compiling on arm64 runner: skip cmake try_run() tests (like aom pie_check)
+# Cross-compiling on arm64 runner for x86_64
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS
     -DCMAKE_SYSTEM_PROCESSOR=x86_64
     -DCMAKE_CROSSCOMPILING=ON
 )
+# Force compiler to target x86_64 so __pie__/__PIE__ macros are correct
+set(VCPKG_C_FLAGS "-arch x86_64")
+set(VCPKG_CXX_FLAGS "-arch x86_64")
