7e3aad3eeea20b7f571ae8fe703467bb35e0a45e
有云转晴 <yyzq-cf@users.noreply.github.com>
2026-07-21 17:26:25 +0800

    Fix macOS x86: disable nasm for aom (arm64 nasm lacks x86_64 multipass support)

--- res/vcpkg-triplets/x64-osx.cmake
+++ res/vcpkg-triplets/x64-osx.cmake
@@ -4,9 +4,11 @@ set(VCPKG_LIBRARY_LINKAGE static)
 set(VCPKG_CMAKE_SYSTEM_NAME Darwin)
 set(VCPKG_OSX_ARCHITECTURES x86_64)
 # Cross-compiling on arm64 runner for x86_64
+# Disable nasm for aom: arm64 nasm doesn't support x86_64 multipass optimization
 set(VCPKG_CMAKE_CONFIGURE_OPTIONS
     -DCMAKE_SYSTEM_PROCESSOR=x86_64
     -DCMAKE_CROSSCOMPILING=ON
+    -DAOM_USE_NASM=OFF
 )
 # Force compiler to target x86_64 so __pie__/__PIE__ macros are correct
 set(VCPKG_C_FLAGS "-arch x86_64")
